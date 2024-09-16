88e0502ee56c6e621fda22d80e75687da66ba6ec
Ahlam <mamboleo1234567@gmail.com>
2024-09-16 13:17:31 +0300

    added new files

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,7 +6,17 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+      template: '%s | Acme Dashboard',
+      default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  }
+
 export default async function Page({
     searchParams,
   }: {
